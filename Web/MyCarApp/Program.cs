496490b54cd4e11dbeabe18441d697cd851323bf
Iliyan Atkov <producenta@gmail.com>
2022-02-06 14:39:00 +0200

    added nlog

--- Web/MyCarApp/Program.cs
+++ Web/MyCarApp/Program.cs
@@ -5,50 +5,67 @@ using Microsoft.EntityFrameworkCore;
 using MyCarApp.Data;
 using MyCarApp.Data.Models;
 using MyCarApp.Services.Constants;
+using NLog;
+using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(args);
-// Add services to the container.
-builder.Services.AddDbContext<MyCarAppDbContext>(options => options.UseSqlServer(builder.Configuration[ConfigurationNamesConstants.ConnectionString]));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
-    .AddEntityFrameworkStores<MyCarAppDbContext>();
-
-builder.Services.AddIdentityServer()
-    .AddApiAuthorization<User, MyCarAppDbContext>();
-
-builder.Services.AddAuthentication()
-    .AddIdentityServerJwt();
-
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
+try
 {
-    app.UseMigrationsEndPoint();
+    var builder = WebApplication.CreateBuilder(args);
+    // Add services to the container.
+    builder.Services.AddDbContext<MyCarAppDbContext>(options => options.UseSqlServer(builder.Configuration[ConfigurationNamesConstants.ConnectionString]));
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+    builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
+        .AddEntityFrameworkStores<MyCarAppDbContext>();
+
+    builder.Services.AddIdentityServer()
+        .AddApiAuthorization<User, MyCarAppDbContext>();
+
+    builder.Services.AddAuthentication()
+        .AddIdentityServerJwt();
+
+    builder.Services.AddControllersWithViews();
+    builder.Services.AddRazorPages();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseMigrationsEndPoint();
+    }
+    else
+    {
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseRouting();
+
+    app.UseAuthentication();
+    app.UseIdentityServer();
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller}/{action=Index}/{id?}");
+    app.MapRazorPages();
+
+    app.MapFallbackToFile("index.html"); ;
+
+    app.Run();
+
 }
-else
+catch (Exception exception)
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    logger.Error(exception, "Stopped program because of exception");
+    throw;
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseIdentityServer();
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-app.MapRazorPages();
-
-app.MapFallbackToFile("index.html"); ;
-
-app.Run();
+finally
+{
+    NLog.LogManager.Shutdown();
+}
